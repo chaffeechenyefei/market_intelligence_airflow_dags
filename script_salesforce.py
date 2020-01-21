85f8e2d0444557e321231ae9714c6d8e765e263b
Yefei Chen <yefei.chen9@wework.com>
2020-01-21 19:14:16 +0800

    +dup check

--- script_salesforce.py
+++ script_salesforce.py
@@ -66,6 +66,7 @@ if __name__ == '__main__':
         'business_name':'company_name',
     })
 
+    sfdnb_lst = sfdnb_lst.drop_duplicates([fid, cid, city], keep='first').reset_index()
     sfdnb_lst.to_csv(pj(datapath_mid,'salesforce_acc_duns_info.csv'))
 
 

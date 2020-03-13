6b9b497b05b012c3e63060371c591c43a342241e
Yefei Chen <yefei.chen9@wework.com>
2020-03-13 17:33:29 +0800

    test

--- one_fun_test.py
+++ one_fun_test.py
@@ -1,8 +1,12 @@
 import os,sys
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
-import salesforce.reason_lib as rslib
+import dnb.reason_lib as rslib
 
 print('##one function test')
-rslib.prod_all_reason_in_one_func()
+print('rslib.prod_prepare_data()')
+rslib.prod_prepare_data()
+#42: Los Angeles
+# print('rslib.prod_all_reason_in_one_func()')
+# rslib.prod_all_reason_in_one_func(ind_city=42)
 print('##Done')
